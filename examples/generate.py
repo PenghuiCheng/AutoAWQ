ad17d8618cec8e4656aed184522f4d2a34fa3b54
Cheng Penghui <penghui.cheng@intel.com>
2024-04-10 05:17:50 -0700

    Support Weight-Only quantization on CPU device with QBits backend
    Signed-off-by: Cheng Penghui <penghui.cheng@intel.com>

--- examples/generate.py
+++ examples/generate.py
@@ -1,11 +1,15 @@
 from awq import AutoAWQForCausalLM
+from awq.utils.utils import get_best_device
 from transformers import AutoTokenizer, TextStreamer
 
 
 quant_path = "TheBloke/Mistral-7B-Instruct-v0.2-AWQ"
 
 # Load model
-model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=True)
+if get_best_device() == "cpu":
+    model = AutoAWQForCausalLM.from_quantized(quant_path, use_qbits=True, fuse_layers=False)
+else:
+    model = AutoAWQForCausalLM.from_quantized(quant_path, fuse_layers=True)
 tokenizer = AutoTokenizer.from_pretrained(quant_path, trust_remote_code=True)
 streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
@@ -19,7 +23,8 @@ prompt = "You're standing on the surface of the Earth. "\
 tokens = tokenizer(
     prompt_template.format(prompt=prompt), 
     return_tensors='pt'
-).input_ids.cuda()
+).input_ids
+tokens = tokens.to(get_best_device())
 
 # Generate output
 generation_output = model.generate(
